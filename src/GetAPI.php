55813565a011058b4f5827476e2bc2f54930394e
Jess Tapley <jjtapley@aol.com>
2020-11-09 16:53:40 +0000

    Created GetAPI class that gets data from API

--- src/GetAPI.php
+++ src/GetAPI.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace TheRealMVP;
+
+
+class GetAPI
+{
+    public function __construct()
+    {
+        $curlConnection = curl_init();
+        curl_setopt($curlConnection, CURLOPT_URL, 'https://dev.maydenacademy.co.uk/resources/sports_teams/sports.json');
+        curl_setopt($curlConnection, CURLOPT_RETURNTRANSFER, true);
+        $apiData = curl_exec($curlConnection);
+        curl_close($curlConnection);
+        $json = json_decode($apiData, true);
+    }
+}
