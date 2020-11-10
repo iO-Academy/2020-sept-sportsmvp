4ca18c237d8ccbec3997c073ae15109e3a90a418
Benjamin Hill <benjaminrshill@hotmail.com>
2020-11-10 10:14:06 +0000

    favicon & initial html

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
+<!DOCTYPE HTML>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#111111">
+
+    <title>The Real MVP</title>
+    <link rel="icon" type="image/x-icon" href="./app/images/favicon.ico">
+    <link type='text/css' rel='stylesheet' href='./app/css/style.css' />
+</head>
+<body>
 <?php
 
 require_once './vendor/autoload.php';
 
 $api = new \TheRealMVP\GetAPI();
+
+?>
+</body>
+</html>
