d195d2576be37a53537da72cca4150ee7072ca8b
Holly Bedford <holly.bedford02@gmail.com>
2020-11-10 14:50:40 +0000

    changed hydrator

--- src/TeamHydrator.php
+++ src/TeamHydrator.php
@@ -6,8 +6,8 @@ class TeamHydrator {
     public static function getData($query)
     {
         $pdo = new PDO('mysql:host=db; dbname=TheRealMVP', 'root', 'password');
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $active_query = $pdo->prepare($query);
+        $active_query->setFetchMode(\PDO::FETCH_CLASS, Team::class, 'data');
         $active_query->execute();
         return $data = $active_query->fetchAll();
     }
