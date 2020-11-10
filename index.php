91cc264d74803ccbd13c3ceae3fa611d36f6b818
Benjamin Hill <benjaminrshill@hotmail.com>
2020-11-10 11:09:26 +0000

    initial html css

--- index.php
+++ index.php
@@ -4,18 +4,123 @@
     <meta charset='UTF-8'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#111111">
-
     <title>The Real MVP</title>
     <link rel="icon" type="image/x-icon" href="./app/images/favicon.ico">
+    <link type='text/css' rel='stylesheet' href='./app/css/normalize.css' />
     <link type='text/css' rel='stylesheet' href='./app/css/style.css' />
 </head>
 <body>
-<?php
+<?php require_once './vendor/autoload.php'; ?>
+<header>
+    <h1>The Real MVP</h1>
+</header>
 
-require_once './vendor/autoload.php';
+<main>
+    <section>
+        <h2>Manchester United</h2>
+        <div class="content">
+            <img src="https://dev.maydenacademy.co.uk/resources/sports_teams/man_utd.png" />
+                <ul>
+                    <li>Sport: Football</li>
+                    <li>Country: UK</li>
+                    <li>Team colours: Red/Yellow</li>
+                </ul>
+        </div>
+    </section>
+    <section>
+        <h2>FC Barcelona</h2>
+        <div class="content">
+            <img src="https://dev.maydenacademy.co.uk/resources/sports_teams/fcb.png" />
+            <div>
+                <ul>
+                    <li>Sport: Football</li>
+                    <li>Country: Spain</li>
+                    <li>Team colours: Red/Yellow/Blue</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section>
+        <h2>FC Barcelona</h2>
+        <div class="content">
+            <img src="https://dev.maydenacademy.co.uk/resources/sports_teams/fcb.png" />
+            <div>
+                <ul>
+                    <li>Sport: Football</li>
+                    <li>Country: Spain</li>
+                    <li>Team colours: Red/Yellow/Blue</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section>
+        <h2>FC Barcelona</h2>
+        <div class="content">
+            <img src="https://dev.maydenacademy.co.uk/resources/sports_teams/fcb.png" />
+            <div>
+                <ul>
+                    <li>Sport: Football</li>
+                    <li>Country: Spain</li>
+                    <li>Team colours: Red/Yellow/Blue</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section>
+        <h2>Manchester United</h2>
+        <div class="content">
+            <img src="https://dev.maydenacademy.co.uk/resources/sports_teams/man_utd.png" />
+                <ul>
+                    <li>Sport: Football</li>
+                    <li>Country: UK</li>
+                    <li>Team colours: Red/Yellow</li>
+                </ul>
+        </div>
+    </section>
+    <section>
+        <h2>FC Barcelona</h2>
+        <div class="content">
+            <img src="https://dev.maydenacademy.co.uk/resources/sports_teams/fcb.png" />
+            <div>
+                <ul>
+                    <li>Sport: Football</li>
+                    <li>Country: Spain</li>
+                    <li>Team colours: Red/Yellow/Blue</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section>
+        <h2>FC Barcelona</h2>
+        <div class="content">
+            <img src="https://dev.maydenacademy.co.uk/resources/sports_teams/fcb.png" />
+            <div>
+                <ul>
+                    <li>Sport: Football</li>
+                    <li>Country: Spain</li>
+                    <li>Team colours: Red/Yellow/Blue</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section>
+        <h2>FC Barcelona</h2>
+        <div class="content">
+            <img src="https://dev.maydenacademy.co.uk/resources/sports_teams/fcb.png" />
+            <div>
+                <ul>
+                    <li>Sport: Football</li>
+                    <li>Country: Spain</li>
+                    <li>Team colours: Red/Yellow/Blue</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</main>
 
-$api = new \TheRealMVP\GetAPI();
-
-?>
+<footer>
+    <img class="logo" src="./app/images/pangologo.png" />
+    &copy; Pangolins <?php echo date('Y'); ?>
+</footer>
 </body>
 </html>
