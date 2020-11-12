69e87301775447bede6673ecbb17fbab36711e32
Arth Pajor <adrasi@gmail.com>
2020-11-12 11:59:22 +0000

    added filtering by sport

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+use TheRealMVP\DBImport;
+use TheRealMVP\DisplayData;
+use TheRealMVP\GetAPI;
+use TheRealMVP\TeamHydrator;
+use TheRealMVP\SportHydrator;
+use TheRealMVP\DisplaySport;
+
+require_once './vendor/autoload.php';
+
+$api = new GetAPI();
+$db = new DBImport();
+$db->storeData($api->getJson());
+$hydrator = TeamHydrator::getData();
+$sportHydrator = SportHydrator::getData();
+
+?>
+
 <!DOCTYPE HTML>
 <html lang='en'>
     <head>
@@ -10,14 +29,6 @@
         <link type='text/css' rel='stylesheet' href='./app/css/style.css' />
     </head>
     <body>
-        <?php
-
-use TheRealMVP\DBImport;
-use TheRealMVP\DisplayData;
-use TheRealMVP\GetAPI;
-use TheRealMVP\TeamHydrator;
-
-require_once './vendor/autoload.php'; ?>
         <header>
             <h1>The Real MVP</h1>
         </header>
@@ -25,31 +36,14 @@ require_once './vendor/autoload.php'; ?>
             <label for="filter">Filter by</label>
             <select>
                 <option value="">All Sports</option>
-                <option value="">Football</option>
-                <option value="">Ice Hockey</option>
-                <option value="">Chess</option>
-                <option value="">Fortnite</option>
-                <option value="">Baseball</option>
-            </select>
-            <select>
-                <option value="">All Countries</option>
-                <option value="">Japan</option>
-                <option value="">Spain</option>
-                <option value="">Russia</option>
-                <option value="">Canada</option>
-                <option value="">United Kingdom</option>
+               <?php echo DisplaySport::displayAllSport($sportHydrator); ?>
             </select>
         <input class="submit" type="submit" value="Submit">
         <input type="submit" value="Clear">
        
         </form>
         <main>
-                <?php 
-                $api = new GetAPI();
-                $db = new DBImport();
-                $db->storeData($api->getJson());
-                $hydrator = TeamHydrator::getData();
-                echo DisplayData::displayAllTeams($hydrator); ?>
+                <?php echo DisplayData::displayAllTeams($hydrator); ?>
         </main>
         <footer>
             <img class="logo" src="./app/images/pangologo.png" />
