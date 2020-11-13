b8478015b175e650bcfbadc48c39e50a2d57a1ee
Richard Catterill <r_catterill@hotmail.com>
2020-11-13 11:23:25 +0000

    Fixed unit tests

--- tests/DisplayFilterTest.php
+++ tests/DisplayFilterTest.php
@@ -3,13 +3,15 @@
 require_once '../vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
+use TheRealMVP\Entities\Sport;
+use TheRealMVP\DisplayHelpers\DisplayFilter;
 
 class DisplayFilterTest extends TestCase
 {
     public function testDisplayFilterSuccess()
     {
         $_SESSION['sport'] = '1';
-        $SportMock = $this->createMock(\TheRealMVP\Sport::class);
+        $SportMock = $this->createMock(\TheRealMVP\Entities\Sport::class);
         $SportMock->expects($this->any())
             ->method('getId')
             ->willReturn(1);
@@ -17,7 +19,7 @@ class DisplayFilterTest extends TestCase
             ->method('getName')
             ->willReturn('Football');
 
-        $result = \TheRealMVP\DisplayFilter::displayFilter('sport', [$SportMock]);
+        $result = \TheRealMVP\DisplayHelpers\DisplayFilter::displayFilter('sport', [$SportMock]);
         $expected = '<option value="1" selected>Football</option>';
         $this->assertEquals($expected, $result);
     }
@@ -25,7 +27,7 @@ class DisplayFilterTest extends TestCase
     public function testDisplayDataFailure()
     {
         $this->expectException(TypeError::class);
-        $result = \TheRealMVP\DisplayFilter::displayFilter('hello');
+        $result = \TheRealMVP\DisplayHelpers\DisplayFilter::displayFilter('hello');
     }
 
 }
